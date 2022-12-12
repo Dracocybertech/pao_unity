f41897e4be3c6f0d09e3ad638e8285e1c25c4800
Ryan Juteau <ryan.juteau@insa-rouen.fr>
2022-12-12 19:18:41 +0100

    Reste à commenter. Prefabs avec bon nom et au bon endroit

--- PAO_Unity/Assets/Scripts/EyeSight.cs
+++ PAO_Unity/Assets/Scripts/EyeSight.cs
@@ -24,36 +24,13 @@ public class EyeSight : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
-
-        // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
             if (hit.collider == lePlayer.GetComponent<CapsuleCollider>())
                 Debug.Log("A détecté le joueur normalement");
         }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            if (hit.collider == lePlayer.GetComponent<CapsuleCollider>())
-                Debug.Log("A détecté le joueur via un rayon de sortie de map");
-        }
     }
-
-    // Activate the Main function when Player enter the trigger area
-    /*void OnTriggerEnter(Collider other)
-    {
-        if (other.name == "Player")
-        {
-            Debug.Log("Joueur entré dans le fov du monstre");
-
-            leMonstreIA.setVueJ();
-
-        }
-
-    }*/
-
 
 
     void OnTriggerStay(Collider other)
